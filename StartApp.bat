d107143432303815bfec3299a6c8e9baeb758cb1
Chemputer <46601333+chemputer@users.noreply.github.com>
2021-05-09 17:35:01 -0400

    Added Twitch colored theme to StartApp.bat with Light Purple on a Black Background

--- StartApp.bat
+++ StartApp.bat
@@ -1,4 +1,6 @@
 @echo off
+REM set color to light purple text on a black background.
+Color 0D
 IF NOT EXIST config.json (
     REM if the config.json file does not exist (assume it hasn't been installed)
     REM then install the twitch watcher program
