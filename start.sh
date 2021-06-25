f7fd328c37cf92bedb6e2be869a9deef658a591e
Chemputer <46601333+chemputer@users.noreply.github.com>
2021-06-25 20:06:23 +0000

    modified start.sh to run npm start, so that pm2 could call it from a cronjob

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-screen -dmS twitch-watcher npm start
-echo "started twitch-watcher, type screen -r to connect to it"
-#screen npm start
+#screen -dmS twitch-watcher npm start
+#echo "started twitch-watcher, type screen -r to connect to it"
+cd /home/turnerb/Twitch-watcher
+npm start
 
